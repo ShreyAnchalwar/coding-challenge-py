60d167b7a9a476a2fe04adfb7c38ad8c3e707999
Shrey Anchalwar <shreyanchalwar@gmail.com>
2024-09-27 15:18:12 +0800

    Update app.py

--- app.py
+++ app.py
@@ -2,9 +2,15 @@ from flask import Flask
 
 app = Flask(__name__)
 
-@app.route('/')
-def hello_world():
-    return 'Hello World!'
+@app.route('/the-clumsy-programmer', methods=['POST'])
+def evaluate():
+    data = request.get_json()
+    logging.info("data sent for evaluation {}".format(data))
+    input_value = data.get("input")
+    result = input_value * input_value
+    logging.info("My result :{}".format(result))
+    return json.dumps(result)
+
 
 if __name__ == '__main__':
     app.run()
