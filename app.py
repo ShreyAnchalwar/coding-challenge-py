39a8ccab720a5281af3ffaea7b3c2b078b62729d
mon772 <56813533+mon772@users.noreply.github.com>
2024-09-27 21:37:00 +0800

    add gpt solution

--- app.py
+++ app.py
@@ -2,9 +2,27 @@ from flask import Flask
 
 app = Flask(__name__)
 
-@app.route('/')
-def hello_world():
-    return 'Hello World!'
+def correct_words(dictionary, mistypes):
+    corrections = []
+    for word in mistypes:
+        for correct_word in dictionary:
+            if len(word) == len(correct_word):
+                diff_count = sum([1 for i in range(len(word)) if word[i] != correct_word[i]])
+                if diff_count == 1:
+                    corrections.append(correct_word)
+                    break
+    return corrections
+
+@app.route('/the-clumsy-programmer', methods=['POST'])
+def correct_words_api():
+    data = request.get_json()
+    results = []
+    for obj in data:
+        dictionary = obj["dictionary"]
+        mistypes = obj["mistypes"]
+        corrections = correct_words(dictionary, mistypes)
+        results.append({"corrections": corrections})
+    return jsonify(results)
 
 if __name__ == '__main__':
     app.run()
